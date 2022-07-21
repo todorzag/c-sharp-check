04f141c52dd1b1053efd23c30117595566381943
todorzag <todorflorida20@gmail.com>
2022-07-21 14:38:01 +0300

    Added Crossing Sequences (WIP) 90/100

--- HarderTasks/CrossingSequences/Program.cs
+++ HarderTasks/CrossingSequences/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CrossingSequences
 {
@@ -6,43 +7,88 @@ namespace CrossingSequences
     {
         static void Main(string[] args)
         {
-            int[] tribonachi = new int[3];
-            ReadTribonachi(tribonachi);
+            int trib1 = int.Parse(Console.ReadLine());
+            int trib2 = int.Parse(Console.ReadLine());
+            int trib3 = int.Parse(Console.ReadLine());
 
-            int spiral1 = int.Parse(Console.ReadLine());
-            int spiral2 = int.Parse(Console.ReadLine());
+            List<int> tribonacci = tribonacciSequence(trib1, trib2, trib3);
 
-            bool found = false;
+            int spiralCurr = int.Parse(Console.ReadLine());
+            int spiralStep = int.Parse(Console.ReadLine());
 
-            while (tribonachi[2] < 1000000 && spiral1 < 1000000)
+            List<int> spiral = spiralSequence(spiralCurr, spiralStep);
+
+            Console.WriteLine(commonElements(tribonacci, spiral));
+        }
+
+        private static List<int> tribonacciSequence (int trib1, int trib2, int trib3)
+        {
+            int tribCurr = trib3;
+            List<int> tribonacci = new List<int> { trib1, trib2, trib3 };
+
+            while (tribCurr < 1000000)
             {
-                int buffer = trib1 + trib2 + trib3;
-                
+                tribCurr = trib1 + trib2 + trib3;
 
-                spiral1 += spiral2;
+                tribonacci.Add(tribCurr);
 
-                if (trib3 == spiral1)
+                trib1 = trib2;
+                trib2 = trib3;
+                trib3 = tribCurr;
+            }
+
+            return tribonacci;
+        }
+
+        private static List<int> spiralSequence(int spiralCurr, int spiralStep)
+        {
+            List<int> spiral = new List<int> { spiralCurr };
+
+            int spiralCount = 0;
+            int spiralStepMul = 1;
+
+            while (spiralCurr < 1000000)
+            {
+                spiralCurr += spiralStep * spiralStepMul;
+
+                spiral.Add(spiralCurr);
+                spiralCount++;
+
+                if (spiralCount % 2 == 0)
                 {
-                    found = true;
-                    break;
+                    spiralStepMul++;
                 }
             }
 
-            if (found)
-                Console.WriteLine($"{trib3}");
-            else
-                Console.WriteLine("No");
+            return spiral;
         }
 
-        private static int[] ReadTribonachi(int[] tribonachi)
+        public static string commonElements(List<int> iArr1, List<int> iArr2)
         {
-            
+            string result = "No";
+            int index2 = 0, index1 = 0;
 
-            tribonachi[0] = int.Parse(Console.ReadLine());
-            tribonachi[1] = int.Parse(Console.ReadLine());
-            tribonachi[2] = int.Parse(Console.ReadLine());
+            while (index2 < iArr2.Count && index1 < iArr1.Count)
+            {
+                int left1 = 0, right1 = (iArr1.Count) - 1;
 
-            return tribonachi;
+                while (left1 <= right1)
+                {
+                    int mid1 = (left1 + right1) / 2;
+                    if (iArr2[index2] == iArr1[mid1])
+                    {
+                        result = $"{iArr1[mid1]}";
+                        return result;
+                    }
+                    else if (iArr2[index2] > iArr1[mid1])
+                        left1 = mid1 + 1;
+                    else right1 = mid1 - 1;
+                }
+
+                index2++; 
+            }
+
+            return result;
         }
     }
 }
